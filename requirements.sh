84be27023c2724e672b1a575c20ca26d7caf73ae
Mr.HaCkEr <59619257+mishabzzz@users.noreply.github.com>
2020-02-14 16:51:43 +0530

    Create requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,16 @@
+toilet -f future installing....
+apt update
+apt upgrade
+apt install git
+apt install python
+apt install python2
+apt install python3
+apt install php
+apt install toilet
+apt install python-pip
+apt install python2-pip
+apt install python3-pip
+apt-get install python3-setuptools
+easy_install3 pip
+
+toilet -f future thanks...
